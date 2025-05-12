c9ae3e02e4cc69567e8b33bf373c11c7912d503c
Sebastián Sanhueza Tapia <sebastian.sanhueza.t@usach.cl>
2025-05-12 15:15:12 -0400

    Creación endpoints, para recibir peticiones mediante los query parametetros de la solicitud

--- src/pages/api/posts/index.ts
+++ src/pages/api/posts/index.ts
@@ -1,16 +1,40 @@
-import type {APIRoute} from 'astro';
-import { getCollection } from 'astro:content';
+import type { APIRoute } from "astro";
+import { getCollection } from "astro:content";
 
 export const prerender = false;
 
 export const GET: APIRoute = async ({ params, request }) => {
+    // const post = await getEntry("blog", params.slug); para buscar solo un post en especifico y no traerlos todos
+	const posts = await getCollection("blog");
 
-    const posts = await getCollection('blog');
+    // construido con query params
+	const url = new URL(request.url);
+	const slug = url.searchParams.get("slug");
 
-    return new Response(JSON.stringify(posts), { 
-        status: 200,
-        headers: {
-            'Content-Type': 'application/json',
-        }
-    });
+	if (slug) {
+		const post = posts.findIndex((post) => post.id === slug);
+		if (post === -1) {
+			return new Response(JSON.stringify({ error: `Post ${slug} not found` }), {
+				status: 404,
+				headers: {
+					"Content-Type": "application/json",
+				},
+			});
+		}
+
+		const postData = posts[post];
+		return new Response(JSON.stringify(postData), {
+			status: 200,
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+	} else {
+		return new Response(JSON.stringify(posts), {
+			status: 200,
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+	}
 };
