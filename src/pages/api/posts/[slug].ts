fc5e41c6a375510d8bb1e4296a6c5ee127a26157
Sebastián Sanhueza Tapia <sebastian.sanhueza.t@usach.cl>
2025-05-15 16:54:19 -0400

    Creación de un las peticiones http CRUD

--- src/pages/api/posts/[slug].ts
+++ src/pages/api/posts/[slug].ts
@@ -4,25 +4,29 @@ import { getStaticPaths } from "../../blog/[...slug].astro";
 
 export const prerender = false;
 
+// GET
 // en este caso se están generando las rutas con los params solicitados al servidor
-export const GET: APIRoute = async ({ params, request,  }) => {
+export const GET: APIRoute = async ({ params, request }) => {
+	const { slug } = params;
 
-    const { slug } = params;
+	const post = await getEntry("blog", slug as any);
 
-    const post = await getEntry("blog", slug as any);
+	if (!post) {
+		return new Response(JSON.stringify({ error: `Post ${slug} not found` }), {
+			status: 404,
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+	}
 
-    if (!post) {
-        return new Response(JSON.stringify({ error: `Post ${slug} not found` }), {
-            status: 404,
-            headers: {
-                "Content-Type": "application/json",
-            },
-        });
-    }
-
-	return new Response(JSON.stringify(post), { status: 200 });
+	return new Response(JSON.stringify(post), {
+		status: 200,
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
 };
-
 
 // export const getStaticPaths: GetStaticPaths = async () => {
 //     return [
@@ -32,3 +36,65 @@ export const GET: APIRoute = async ({ params, request,  }) => {
 //         }
 //     ]
 // }
+
+// POST 
+export const POST: APIRoute = async ({ params, request }) => {
+    const body = await request.json();
+
+	return new Response(JSON.stringify({
+        method: "POST",
+        ...body,
+    }), {
+		status: 200,
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
+};
+
+// PUT
+export const PUT: APIRoute = async ({ params, request }) => {
+    const body = await request.json();
+
+	return new Response(JSON.stringify({
+        method: "PUT",
+        ...body,
+    }), {
+		status: 200,
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
+};
+
+// PATCH
+export const PATCH: APIRoute = async ({ params, request }) => {
+    const body = await request.json();
+
+    return new Response(JSON.stringify({
+        method: "PATCH",
+        ...body,
+    }), {
+        status: 200,
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+};
+
+// DELETE
+export const DELETE: APIRoute = async ({ params, request }) => {
+    const { slug } = params;
+
+    const body = await request.json();
+
+    return new Response(JSON.stringify({
+        method: "DELETE",
+        slug: `slug ${slug} deleted`,
+    }), {
+        status: 200,
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+};
