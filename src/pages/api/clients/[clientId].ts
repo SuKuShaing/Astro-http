2dc6a3fc5721d1d1e3c51ed4f611a3b008b05ba9
Sebastián Sanhueza Tapia <sebastian.sanhueza.t@usach.cl>
2025-05-19 18:11:19 -0400

    alteración del formato por prettier

--- src/pages/api/clients/[clientId].ts
+++ src/pages/api/clients/[clientId].ts
@@ -1,91 +1,136 @@
 import type { APIRoute, GetStaticPaths } from "astro";
+import { Clients, db } from "astro:db";
 
 export const prerender = false;
 
 // GET
 // en este caso se están generando las rutas con los params solicitados al servido
 export const GET: APIRoute = async ({ params, request }) => {
-    // entre corchetes puedo usar cualquier nombre, el que mejor describa el parámetro que esperas en la URL
+	// entre corchetes puedo usar cualquier nombre, el que mejor describa el parámetro que esperas en la URL
 	const { clientId } = params;
 
-	return new Response(JSON.stringify({
-        method: "GET",
-        clientId: 1,
-        slug: clientId,
-    }), {
-		status: 200,
-		headers: {
-			"Content-Type": "application/json",
-		},
-	});
+	return new Response(
+		JSON.stringify({
+			method: "GET",
+			clientId: 1,
+			slug: clientId,
+		}),
+		{
+			status: 200,
+			headers: {
+				"Content-Type": "application/json",
+			},
+		}
+	);
 };
 
-// POST 
+// POST
 export const POST: APIRoute = async ({ params, request }) => {
-    const { clientId } = params;
-    const body = await request.json();
+	const { clientId } = params;
+	const body = await request.json();
 
-	return new Response(JSON.stringify({
-        method: "POST",
-        clientId: 1,
-        slug: clientId,
-        ...body,
-    }), {
-		status: 200,
-		headers: {
-			"Content-Type": "application/json",
-		},
-	});
+	return new Response(
+		JSON.stringify({
+			method: "POST",
+			clientId: 1,
+			slug: clientId,
+			...body,
+		}),
+		{
+			status: 200,
+			headers: {
+				"Content-Type": "application/json",
+			},
+		}
+	);
 };
 
 // PUT
 export const PUT: APIRoute = async ({ params, request }) => {
-    const { clientId } = params;
-    const body = await request.json();
+	const { clientId } = params;
+	const body = await request.json();
 
-	return new Response(JSON.stringify({
-        method: "PUT",
-        clientId: 1,
-        slug: clientId,
-        ...body,
-    }), {
-		status: 200,
-		headers: {
-			"Content-Type": "application/json",
-		},
-	});
+	return new Response(
+		JSON.stringify({
+			method: "PUT",
+			clientId: 1,
+			slug: clientId,
+			...body,
+		}),
+		{
+			status: 200,
+			headers: {
+				"Content-Type": "application/json",
+			},
+		}
+	);
 };
 
 // PATCH
 export const PATCH: APIRoute = async ({ params, request }) => {
     const { clientId } = params;
-    const body = await request.json();
 
-    return new Response(JSON.stringify({
-        method: "PATCH",
-        clientId: 1,
-        slug: clientId,
-        ...body,
-    }), {
-        status: 200,
-        headers: {
-            "Content-Type": "application/json",
-        },
-    });
+	try {
+		const { id, ...body } = await request.json();
+
+		const resp = await db.insert(Clients).values(body);
+		console.log("🚀 ~ constPOST:APIRoute= ~ resp:", resp);
+		const { lastInsertRowid } = resp;
+
+		return new Response(
+			JSON.stringify({
+				method: "POST",
+				id: +lastInsertRowid!.toString(),
+				...body,
+			}),
+			{
+				status: 200,
+				headers: {
+					"Content-Type": "application/json",
+				},
+			}
+		);
+	} catch (error) {
+		console.error("🚀 ~ constPOST:APIRoute= ~ error:", error);
+		return new Response(JSON.stringify({ msg: "No body found" }), {
+			status: 400,
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+	}
+
+	// const { clientId } = params;
+	// const body = await request.json();
+
+	// return new Response(JSON.stringify({
+	//     method: "PATCH",
+	//     clientId: 1,
+	//     slug: clientId,
+	//     ...body,
+	// }), {
+	//     status: 200,
+	//     headers: {
+	//         "Content-Type": "application/json",
+	//     },
+	// });
 };
 
 // DELETE
 export const DELETE: APIRoute = async ({ params, request }) => {
-    const { clientId } = params;
+	const { clientId } = params;
 
-    return new Response(JSON.stringify({
-        method: "DELETE",
-        clientId: 1,
-        slug: clientId,
-    }), {
-        status: 200,
-        headers: {
-            "Content-Type": "application/json",
-        },
-    });
+	return new Response(
+		JSON.stringify({
+			method: "DELETE",
+			clientId: 1,
+			slug: clientId,
+		}),
+		{
+			status: 200,
+			headers: {
+				"Content-Type": "application/json",
+			},
+		}
+	);
 };
