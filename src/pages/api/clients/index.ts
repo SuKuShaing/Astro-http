2dd37219fdf7a8491c291d341c81416b47cb66a3
Sebastián Sanhueza Tapia <sebastian.sanhueza.t@usach.cl>
2025-05-19 17:55:28 -0400

    Se implementan en los métodos GET y POST de la API de clientes la obtención y creación de registros en la base de datos.

--- src/pages/api/clients/index.ts
+++ src/pages/api/clients/index.ts
@@ -1,12 +1,15 @@
 import type { APIRoute } from "astro";
+import { Clients, db } from "astro:db";
 
 export const prerender = false;
 
 export const GET: APIRoute = async ({ params, request }) => {
+
+	// select * from Clients where ...
+	const users = await db.select().from(Clients);
+
 	return new Response(
-		JSON.stringify({
-			method: "GET",
-		}),
+		JSON.stringify(users),
 		{
 			status: 200,
 			headers: {
@@ -16,16 +19,44 @@ export const GET: APIRoute = async ({ params, request }) => {
 	);
 };
 
+
+
 export const POST: APIRoute = async ({ params, request }) => {
-	return new Response(
-		JSON.stringify({
-			method: "POST",
-		}),
-		{
-			status: 200,
-			headers: {
-				"Content-Type": "application/json",
-			},
-		}
-	);
+	try {
+		// const body = await request.json();
+		const { id, ...body } = await request.json();
+		/* Al destructurar el body, podemos sacar el id y el resto de los datos, el resto de los datos quedan agruapados en body,
+		podemos pasar el body como respuesta y al haber sacado el id, no lo incluimos en la respuesta.
+		Esto es útil para no enviar datos sensibles o innecesarios al cliente, o usar el id para hacer una consulta a la base de datos o cosas así.
+		*/
+
+		const resp = await db.insert(Clients).values(body)
+		console.log("🚀 ~ constPOST:APIRoute= ~ resp:", resp)
+		const { lastInsertRowid } = resp;
+
+		return new Response(
+			JSON.stringify({
+				method: "POST",
+				id: +lastInsertRowid!.toString(),
+				...body,
+			}),
+			{
+				status: 200,
+				headers: {
+					"Content-Type": "application/json",
+				},
+			}
+		);
+	} catch (error) {
+		console.error("🚀 ~ constPOST:APIRoute= ~ error:", error)
+		return new Response(
+			JSON.stringify({msg: 'No body found'}),
+			{
+				status: 400,
+				headers: {
+					"Content-Type": "application/json",
+				},
+			}
+		);
+	}
 };
